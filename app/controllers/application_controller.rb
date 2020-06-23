b16edc248174ffdefc489637f959839d3dfe73f2
MarkDLR <mark.p.dlr@gmail.com>
2020-06-23 17:27:49 -0500

    Mark

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < Sinatra::Base
 
     set(:views, 'app/views')
-    
 
+    get('/airline-list') do
+        @airlines = Airline.all
+        erb(:list_airlines)
+    end
+
+    get('/get-airline/:id') do
+        @airline = Airline.find(params[:id])
+        #@tickets = Ticket.where(airline_id: airline.id)
+        erb(:single_airline)
+    end
+
+    post('/remove/:id') do
+        @airline = Airline.find(params[:id])
+        @airline.destroy()
+    end
 end
 
 
